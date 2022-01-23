01c74170dcce1dbd286f65e6cb4f5333297873cd
Kevv_in <80095257+ksowah@users.noreply.github.com>
2022-01-23 11:30:18 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Left from './components/Left';
+import Main from './components/Main';
+
+function App() {
+  return (
+      <div className='app'>
+          <Left/>
+          <Main/>
+      </div>
+  );
+}
+
+export default App;
